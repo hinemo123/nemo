2aaae7237c60198437593f8023be1cbfd66ecc31
hinemo123 <49576257+hinemo123@users.noreply.github.com>
2019-07-06 09:32:53 +0700

    Add files via upload

---
+++ nemo_gui.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+import requests
+import nemo
+
+HEIGHT = 500
+WIDTH = 600
+
+def test_function(entry):
+	print("This is the entry:", entry)
+
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# a4aa5e3d83ffefaba8c00284de6ef7c3
+
+def format_response(weather):
+	try:
+		name = weather['name']
+		desc = weather['weather'][0]['description']
+		temp = weather['main']['temp']
+
+		final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (name, desc, temp)
+	except:
+		final_str = 'There was a problem retrieving that information'
+
+	return final_str
+
+def get_weather(city):
+	weather_key = 'a4aa5e3d83ffefaba8c00284de6ef7c3'
+	url = 'https://api.openweathermap.org/data/2.5/weather'
+	params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+	response = requests.get(url, params=params)
+	weather = response.json()
+
+	label['text'] = format_response(weather)
+
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+a=tk.StringVar()
+entry = tk.Entry(frame, font=40,textvariable=a)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Question", font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+nemo.Greeting()
+b=nemo.ask()
+a.set(b)
+if "open" in b:
+	import text
+	if "Facebook" in b:
+		text.open_facebook()
+	elif "Gmail" in b:
+		text.open_gmail()
+	elif "GitHub" in b:
+		text.open_github()
+	elif "Wiki" in b:
+		text.open_wiki
+	elif "song" in b and "YouTube" in b:
+		song=b.split()[1]
+		text.open_video_on_youtube(song)
+	else:
+		link=b.replace("open","")
+		text.open_google(link)
+
+root.mainloop()
